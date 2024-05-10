51b704ffd405e299f7afe844e576d5c60ea52adf
UpendraTuttagunta <upendratuttagunta@gmail.com>
2024-05-10 20:21:35 +0530

    functions

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from Current Script"
 echo "Before calling others script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./16-others-script.sh
+./16-others-script.sh
 
 echo "After calling others script, course: $COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="DevOps from Other Script"
+
+echo "Variable value from Other scripts: $COURSE"
+echo "Process ID of other script: $$"
